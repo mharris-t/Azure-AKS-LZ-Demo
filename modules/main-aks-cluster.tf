8748a1a71748c669fe166f6183e3dd9ebdc446a4
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2022-10-26 21:46:45 +0300

    adding required version

--- modules/main-aks-cluster.tf
+++ modules/main-aks-cluster.tf
@@ -90,5 +90,19 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
