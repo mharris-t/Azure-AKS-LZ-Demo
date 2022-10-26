9a83ea2ad7f74ca6e1056e30b0a77dbc83563c91
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2022-10-26 22:58:31 +0300

    fix indentation

--- modules/main-aks-cluster.tf
+++ modules/main-aks-cluster.tf
@@ -4,17 +4,17 @@ resource "azurerm_resource_group" "aks_rg" {
 }
 
 data "azurerm_kubernetes_service_versions" "current" {
-  location = azurerm_resource_group.aks_rg.location
+  location        = azurerm_resource_group.aks_rg.location
   include_preview = false
 }
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "${var.cluster_name}-cluster"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "${azurerm_resource_group.aks_rg.name}-cluster"
-  kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
-  node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
+  name                   = "${var.cluster_name}-cluster"
+  location               = azurerm_resource_group.aks_rg.location
+  resource_group_name    = azurerm_resource_group.aks_rg.name
+  dns_prefix             = "${azurerm_resource_group.aks_rg.name}-cluster"
+  kubernetes_version     = data.azurerm_kubernetes_service_versions.current.latest_version
+  node_resource_group    = "${azurerm_resource_group.aks_rg.name}-nrg"
 
   default_node_pool {
     name                 = "${var.cluster_name}-nodepool"
